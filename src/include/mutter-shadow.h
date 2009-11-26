e70fbbcc8b068cbc5b6cd565840085e6deb42ac5
Tomas Frydrych <tf@linux.intel.com>
2009-11-26 12:54:33 +0000

    [MutterShadow] An API allowing plugins to generate their custom shadows
    The commmit adds new vfunction get_shadow() to MutterPluginClass, allowing
    plugins to replace the stock gaussian shadow with a custom actor. The
    shadow is encapsulated in a MutterShadow structure that contains the custom
    actor, and four points of attachment describing the position and size of the
    shadow relative to the window.
    
    In addition, this commit replaces the MutterWindow::no-frame boolean property
    with shape-type emum property which allows to force and disable the shadow
    on a given window, or leave the compositor to decide.
    
    The plugin API version number is bumped by one.

---
+++ src/include/mutter-shadow.h
@@ -0,0 +1,37 @@
+#ifndef MUTTER_SHADOW_H
+#define MUTTER_SHADOW_H
+
+#include <clutter/clutter.h>
+#include "compositor.h"
+
+/**
+ * MutterShadowType:
+ * @MUTTER_SHADOW_AUTOMATIC: The compositor decides whether window should
+ * have shadow based on window type
+ * @MUTTER_SHADOW_ALWAYS: The window should have shadow
+ * @MUTTER_SHADOW_NEWER:  The window should not have a shadow
+ */
+typedef enum
+{
+  MUTTER_SHADOW_AUTOMATIC,
+  MUTTER_SHADOW_ALWAYS,
+  MUTTER_SHADOW_NEVER
+} MutterShadowType;
+
+typedef struct _MutterShadow MutterShadow;
+
+struct _MutterShadow
+{
+  ClutterActor *actor;
+  gint          attach_left;
+  gint          attach_top;
+  gint          attach_right;
+  gint          attach_bottom;
+};
+
+MutterShadow * mutter_shadow_new               (void);
+void           mutter_shadow_free              (MutterShadow *shadow);
+MutterShadow * mutter_shadow_create_for_window (MetaCompositor *compositor,
+                                                MutterWindow   *window);
+
+#endif /* MUTTER_SHADOW_H */
