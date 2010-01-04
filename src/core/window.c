6f5ad12a0d78b3dca23bd9581fb7c547e3c4d475
Tomas Frydrych <tf@linux.intel.com>
2010-01-04 14:05:04 +0000

    Do not load fallback icons
    Fallback window icons are useless (the whole point of the icons is to identify
    the window by them) and just waste resources.

--- src/core/window.c
+++ src/core/window.c
@@ -6384,8 +6384,10 @@ meta_window_update_icon_now (MetaWindow *window)
       redraw_icon (window);
     }
 
+#if 0
   g_assert (window->icon);
   g_assert (window->mini_icon);
+#endif
 }
 
 static gboolean
